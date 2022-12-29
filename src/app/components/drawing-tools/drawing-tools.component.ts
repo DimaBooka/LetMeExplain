e8ad0b0c428fe67f1111f94a3a8e57c705921659
Dima Booka <atlasb777@gmail.com>
2022-12-29 21:34:19 +0000

    feat: add drawing tools

--- src/app/components/drawing-tools/drawing-tools.component.ts
+++ src/app/components/drawing-tools/drawing-tools.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core'
 
 @Component({
   selector: 'app-drawing-tools',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./drawing-tools.component.scss']
 })
 export class DrawingToolsComponent {
+  colors: string[] = [
+    '#fff',
+    '#000',
+    '#15c9d9',
+    '#03bb56',
+    '#fd7200',
+    '#fa2c11',
+  ]
+  lineWidths: number[] = [5, 10, 15, 20]
 
+  eraserColor = '#2b2b2b'
+  eraserLineWidth = this.lineWidths[3]
+
+  @Input() selectedColor: string = this.colors[0]
+  @Input() selectedLineWidth: number = this.lineWidths[0]
+
+  @Output() setColor: EventEmitter<string> = new EventEmitter<string>()
+  @Output() setLineWidth: EventEmitter<number> = new EventEmitter<number>()
+
+  onSetColor(color: string) {
+    if (this.selectedLineWidth == this.eraserLineWidth && this.selectedColor === this.eraserColor) {
+      this.setLineWidth.emit(this.lineWidths[0])
+    }
+
+    this.setColor.emit(color)
+  }
+
+  setEraser() {
+    this.setColor.emit(this.eraserColor)
+    this.setLineWidth.emit(this.eraserLineWidth)
+  }
 }
--- src/app/pages/create-explanation/create-explanation.component.ts
+++ src/app/pages/create-explanation/create-explanation.component.ts
@@ -51,15 +51,8 @@ export class CreateExplanationComponent implements AfterViewInit {
     this.cdr.markForCheck()
   }
 
-  colors: string[] = [
-    '#fff',
-    '#03bb56',
-    '#fd7200',
-    '#fa2c11',
-    '#000',
-    '#2b2b2b'
-  ]
-  lastSelectedColor: string = this.colors[0]
+  lastSelectedColor: string = '#fff'
+  lastSelectedLineWidth: number = 5
 
   constructor(private cdr: ChangeDetectorRef, private slidesService: SlidesService) {}
 
@@ -74,8 +67,8 @@ export class CreateExplanationComponent implements AfterViewInit {
     this.slidesService.editorContext = this.editor.nativeElement.getContext('2d')
     this.slidesService.slides = [{ name: 'Slide #1', dataURL: '' }]
 
-    this.onSetColor(this.colors[0])
-    this.onSetLineWidth(5)
+    this.onSetColor(this.lastSelectedColor)
+    this.onSetLineWidth(this.lastSelectedLineWidth)
 
     if (!this.slidesService.editorContext) {
       return
@@ -89,7 +82,9 @@ export class CreateExplanationComponent implements AfterViewInit {
     if (!this.slidesService.editorContext) {
       return
     }
+    this.lastSelectedLineWidth = width
     this.slidesService.editorContext.lineWidth = width
+
     this.cdr.markForCheck()
   }
 
@@ -100,107 +95,6 @@ export class CreateExplanationComponent implements AfterViewInit {
     this.lastSelectedColor = color
     this.slidesService.editorContext.strokeStyle = color
 
-    // for eraser let's use bigger radius
-    this.onSetLineWidth(this.colors[this.colors.length - 1] === color ? 15 : 5)
-
     this.cdr.markForCheck()
   }
-
-  // onClear() {
-  //   if (!this.editor) {
-  //     return
-  //   }
-  //   this.editorContext?.clearRect(0, 0, this.editor.nativeElement.width, this.editor.nativeElement.height)
-  //   this.cdr.markForCheck()
-  // }
-
-  // onChangeSlide(index: number) {
-  //   if (!this.editor) {
-  //     return
-  //   }
-  //
-  //   // save current progress to slide
-  //   this.slides[this.currentSlideIndex].dataURL = this.editor.nativeElement.toDataURL('imag/png')
-  //
-  //   this.drawSlide(index)
-  // }
-  //
-  // onNextSlide(clear?: boolean) {
-  //   if (!this.editor) {
-  //     return
-  //   }
-  //
-  //   this.slides[this.currentSlideIndex].dataURL = this.editor.nativeElement.toDataURL('imag/png')
-  //   if (clear) {
-  //     this.onClear()
-  //   }
-  //   this.slides.push({ name: `Slide #${this.slides.length + 1}`, dataURL: '' })
-  //   this.currentSlideIndex = this.slides.length - 1
-  //   this.cdr.markForCheck()
-  // }
-  //
-  // onRemoveSlide(index: number) {
-  //   this.slides.splice(index, 1)
-  //
-  //   // if removed slide is currently selected slide
-  //   if (this.currentSlideIndex === index) {
-  //     //if slides available then take first slide near removed
-  //     if (this.slides.length > 0) {
-  //
-  //       // next otherwise prev slide
-  //       let newIndex = this.slides.length - 1 >= index ? index : index - 1
-  //
-  //       // negative value, then last available
-  //       this.drawSlide(newIndex >= 0 ? newIndex : this.slides.length - 1)
-  //     }
-  //     //if no slides available then create new
-  //     else {
-  //       this.onClear()
-  //       this.slides.push({ name: `Slide #${this.slides.length + 1}`, dataURL: '' })
-  //       this.currentSlideIndex = this.slides.length - 1
-  //     }
-  //   }
-  //   // if not - then update index value
-  //   else {
-  //     // remove 1 when removed slide before current
-  //     this.currentSlideIndex -= this.currentSlideIndex > index ? 1 : 0
-  //   }
-  //
-  //   this.cdr.markForCheck()
-  // }
-  //
-  // drawSlide(index: number) {
-  //   const slide = this.slides[index]
-  //   this.currentSlideIndex = index
-  //
-  //   const img = new Image()
-  //   img.onload = () => {
-  //     this.onClear()
-  //     this.editorContext?.drawImage(img, 0, 0)
-  //   }
-  //   img.src = slide.dataURL
-  //   this.cdr.markForCheck()
-  // }
-  //
-  // onDownload() {
-  //   if (!this.editor) {
-  //     return
-  //   }
-  //
-  //   this.slides[this.currentSlideIndex].dataURL = this.editor.nativeElement.toDataURL('imag/png')
-  //
-  //   const data = this.slides.map(s => s.dataURL).map((s, i) => `${i > 0 ? '---\n' : ''}![image](${s})\n`).join('')
-  //
-  //   let a = document.createElement('a')
-  //   a.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(data)
-  //   a.download = 'explanation.md'
-  //   a.click()
-  //
-  //   this.cdr.markForCheck()
-  // }
-  //
-  // onUpload(fileContent: string) {
-  //   this.slidesService.parseSlides(fileContent)
-  //   this.slides = this.slidesService.slides
-  // }
 }
